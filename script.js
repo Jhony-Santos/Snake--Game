cefe4026164a6114d46570a941447b09456f44c6
Jhonatan Santos <jhony.jpn@gmail.com>
2021-01-18 21:46:22 -0300

    controle de movimentos realizados

--- script.js
+++ script.js
@@ -22,7 +22,26 @@ function createSnake(){
     }
 }
 
+document.addEventListener('keydown', update);
+
+function update(event){
+    if(event.keyCode == 37 && direction!="right"){direction="left"};
+    if(event.keyCode == 38 && direction!="down"){direction="up"};
+    if(event.keyCode==39 && direction!="left"){direction="right"};
+    if(event.keyCode==40 && direction!="up"){direction="down"};
+
+
+}
+
+
+
+
 function startGame(){
+    if(snake[0].x > 15 * box && direction=="right"){snake[0].x = 0}
+    if(snake[0].x < 0 &&  direction =="left"){ snake[0].x = 16 * box}
+    if(snake[0].y >15 * box && direction=="down"){ snake[0].y = 0}
+    if(snake[0].y < 0  && direction=="up"){snake[0].y = 16 * box}
+
     criarBG();
     createSnake();
 
@@ -42,10 +61,11 @@ function startGame(){
     }
 
     snake.unshift(newHead);
-
-
 }
 
 let game=setInterval(startGame,100);
+update();
+
+
 
 
